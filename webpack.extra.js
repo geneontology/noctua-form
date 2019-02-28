ca149336580a3a47300e1d9a20dc8d2c5185bdd9
tmushayahama <tmushayahama@gmail.com>
2019-02-28 02:15:08 -0800

    fix webpack-extra htpp regex

--- webpack.extra.js
+++ webpack.extra.js
@@ -2,6 +2,6 @@ const webpack = require('webpack');
 module.exports = {
   plugins: [
     new webpack.IgnorePlugin(/ringo\/httpclient/),
-    new webpack.IgnorePlugin(/http/)
+    new webpack.IgnorePlugin(/^http$/)
   ]
 }
