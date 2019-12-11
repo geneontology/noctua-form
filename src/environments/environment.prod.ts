a119a49d5d6001568ae79c2c781c126a584c23b4
tmushayahama <tmushayahama@gmail.com>
2019-12-11 04:43:26 -0800

    temporary fix for barista dev url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,15 +1,32 @@
+// This file can be replaced during build by using the `fileReplacements` array.
+// `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
+// The list of file replacements can be found in `angular.json`.
+
 export const environment = {
-  production: true,
-  spaqrlApiUrl: 'http://rdf.geneontology.org/blazegraph/sparql',
+  production: false,
+  spaqrlApiUrl: 'http://rdf-internal.berkeleybop.io/blazegraph/sparql',
+  // spaqrlApiUrl: 'http://rdf.geneontology.org/blazegraph/sparql',
   // gorestApiUrl: 'https://api.geneontology.cloud/'
   gorestApiUrl: 'http://localhost:3000/',
-  globalGolrServer: ' http://noctua-golr.berkeleybop.org/',//'http://amigo-dev-golr.berkeleybop.org/',
+  globalGolrServer: ' http://noctua-golr.berkeleybop.org/',
   globalGolrCompanionServer: ' http://golr.berkeleybop.org/',
   globalGolrNeoServer: 'http://noctua-golr.berkeleybop.org/',
   globalMinervaDefinitionName: 'minerva_public_dev',
-  globalBaristaLocation: 'http://barista-dev.berkeleybop.org',
+  globalBaristaLocation: 'http://barista.berkeleybop.org',
   //Workbench
-  noctuaUrl: 'http://noctua-dev.berkeleybop.org/',
-  workbenchUrl: 'http://noctua-dev.berkeleybop.org//workbench/',
+  noctuaUrl: `${window.location.origin}`,
+  workbenchUrl: `${window.location.origin}/workbench/`,
 
+  locationStoreApi: 'https://6xq2j25tah.execute-api.us-east-1.amazonaws.com/dev',
+  amigoTerm: 'http://amigo.geneontology.org/amigo/term/',
+  wikidataSparqlUrl: 'https://query.wikidata.org/sparql',
+  pubMedSummaryApi: 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id='
 };
+
+/*
+ * In development mode, to ignore zone related error stack frames such as
+ * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
+ * import the following file, but please comment it out in production mode
+ * because it will have performance impact when throw error
+ */
+// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
