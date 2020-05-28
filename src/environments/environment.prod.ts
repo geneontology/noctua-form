648837ddda3cfc1600e73632bee54c773ed6d54f
tmushayahama <tmushayahama@gmail.com>
2020-05-27 21:08:02 -0700

    updated to nf-base 0.7.72  endpoints to globals https://github.com/geneontology/noctua-form/issues/101

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,7 @@
 // `ng build ---prod` replaces `environment.ts` with `environment.prod.ts`.
 // The list of file replacements can be found in `angular.json`.
 
+import * as enviromnenetData from './environment-data';
 
 declare var global_barista_location: any;
 declare var global_minerva_definition_name: any;
@@ -10,38 +11,29 @@ declare var global_workbenches_model: any;
 
 const baristaLocation = typeof global_barista_location !== 'undefined' ? global_barista_location : 'http://barista-dev.berkeleybop.org';
 const minervaDefinitionName = typeof global_minerva_definition_name !== 'undefined' ? global_minerva_definition_name : 'minerva_public_dev';
-const globalWorkbenchesModel = typeof global_workbenches_model !== 'undefined' ? global_workbenches_model : [];
-const globalWorkbenchesUniversal = typeof global_workbenches_universal !== 'undefined' ? global_workbenches_universal : [];
+const globalWorkbenchesModel = typeof global_workbenches_model !== 'undefined'
+  ? global_workbenches_model
+  : enviromnenetData.globalWorkbenchesModel;
+const globalWorkbenchesUniversal = typeof global_workbenches_universal !== 'undefined'
+  ? global_workbenches_universal
+  : enviromnenetData.globalWorkbenchesUniversal;
+
 
 export const environment = {
-  production: false,
+  production: true,
   spaqrlApiUrl: 'http://rdf-internal.berkeleybop.io/blazegraph/sparql',
-  // spaqrlApiUrl: 'http://rdf.geneontology.org/blazegraph/sparql',
-  // gorestApiUrl: 'https://api.geneontology.cloud/'
-  gorestApiUrl: 'http://localhost:3000/',
-  globalGolrServer: ' http://noctua-golr.berkeleybop.org/',
-  globalGolrCompanionServer: ' http://golr.berkeleybop.org/',
-  globalGolrNeoServer: 'http://noctua-golr.berkeleybop.org/',
   globalMinervaDefinitionName: minervaDefinitionName,
   globalBaristaLocation: baristaLocation,
   globalWorkbenchesModel: globalWorkbenchesModel,
   globalWorkbenchesUniversal: globalWorkbenchesUniversal,
+  searchApi: `${baristaLocation}/search/`,
 
   //Workbench
   noctuaUrl: `${window.location.origin}`,
   workbenchUrl: `${window.location.origin}/workbench/`,
-
-  searchApi: `${baristaLocation}/search/`,
 
   amigoTerm: 'http://amigo.geneontology.org/amigo/term/',
   wikidataSparqlUrl: 'https://query.wikidata.org/sparql',
   pubMedSummaryApi: 'https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esummary.fcgi?db=pubmed&retmode=json&id='
 };
 
-/*
- * In development mode, to ignore zone related error stack frames such as
- * `zone.run`, `zoneDelegate.invokeTask` for easier debugging, you can
- * import the following file, but please comment it out in production mode
- * because it will have performance impact when throw error
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
